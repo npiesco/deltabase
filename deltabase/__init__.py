18340b58b826d16027a727c68b6d5df8cc722add
dm <118793435+uname-n@users.noreply.github.com>
2024-08-18 22:26:09 -0700

    data_page_size_limit

--- deltabase/__init__.py
+++ deltabase/__init__.py
@@ -252,7 +252,7 @@ class delta:
         table_path = join(self.__delta_source, database, table)
         data = self.sql(f"select * from {table}", dtype="polars")
         
-        options = dict(mode="overwrite", delta_write_options=dict(writer_properties=()))
+        options = dict(mode="overwrite", delta_write_options=dict(writer_properties=dict()))
         if partition_by: options["delta_write_options"]["partition_by"] = partition_by
         if force: options["delta_write_options"]["schema_mode"] = "overwrite"
         if data_page_size_limit: options["delta_write_options"]["writer_properties"]["data_page_size_limit"] = data_page_size_limit
